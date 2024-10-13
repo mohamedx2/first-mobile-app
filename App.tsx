97d828f0cb8aff1465407a801d21ed67e7d96504
mohamedx2 <hamroun1969@gmail.com>
2024-10-13 10:41:20 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>you know nothing about houman capacities</Text>
       <StatusBar style="auto" />
     </View>
   );
